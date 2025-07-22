4af44387fc659a1a91a890380a746fb40b61405e
ClassicalExplosion <85848810+flybot2010-lab@users.noreply.github.com>
2025-07-22 16:55:27 -0500

    Public Release

--- main.py
+++ main.py
@@ -2,24 +2,32 @@
 import paramiko
 import configparser
 import threading
+from typing import Dict, List
 import schedule
 import datetime
 import time as time_py
 
 # End Imports
-print("Intellicue\n(C) 2025 9D Crew")
+intellilogo = r"""
+ ___       _       _ _ _                 
+|_ _|_ __ | |_ ___| | (_) ___ _   _  ___ 
+ | || '_ \| __/ _ \ | | |/ __| | | |/ _ \
+ | || | | | ||  __/ | | | (__| |_| |  __/
+|___|_| |_|\__\___|_|_|_|\___|\__,_|\___|
+"""
+print(intellilogo)
+print("(C) 2025 9D Crew")
 
 # Config Loading
-config = configparser.ConfigParser(); config.read('config.ini') # Load Config File
-global ip; global port; global username; global password; global lot8s; global ldl; global run_time # Global Everything
-# Load Config Into Python Variables
-ip = config['SSH']['IP']
-port = config['SSH']['PORT']; port = int(port)
-username = config['SSH']['USERNAME']; password = config['SSH']['PASSWORD']
-lot8s = config['FLAVOR']['LOT8S']; ldl = config['FLAVOR']['LDL']
-run_time = config['FLAVOR']['RUN_TIME']; run_time = int(run_time)
-print("Config Loaded Successfully")
-print("Using LOT8s Flavor", lot8s, "And LDL Flavor", ldl)
+print("Loading config..")
+config = configparser.ConfigParser(); config.read('config.ini')
+ssh_config = config['SSH']; flavor_config = config['FLAVOR']
+ip = ssh_config.get('IP'); port = ssh_config.getint('PORT')
+username = ssh_config.get('USERNAME'); password = ssh_config.get('PASSWORD')
+lot8s = flavor_config.get('LOT8S'); ldl = flavor_config.get('LDL')
+run_time = flavor_config.getint('RUN_TIME')
+print("Config Loaded Successfully.")
+print(f"Using LOT8s Flavor: {lot8s} and LDL Flavor: {ldl}")
 
 # i1 Connection
 print("Connecting to i1..")
@@ -28,14 +36,15 @@ ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(ip, port=port, username=username, password=password, look_for_keys=False, allow_agent=False)
 shell = ssh_client.invoke_shell()
 
-def password_man(): # No clue what this does, but it's in i1-encoder so wth
+def password_man(): # this is in i1-encoder, so its probably important..
     global shell
     while True:
         output = shell.recv(1024).decode()
         if "Password:" in output:
             shell.send("i1\n")
+
 threading.Thread(target=password_man, daemon=True).start()
-shell.send("su -l dgadmin\n") # Switch user to dgadmin
+shell.send("su -l dgadmin\n") # switch to dgadmin
 
 def cmd(command):
     global shell
@@ -43,9 +52,9 @@ def cmd(command):
     shell.send(command + "\n")
 
 print("i1 Connected.")
-cmd("runomni /twc/util/toggleNationalLDL.pyc 0") # In case LDL is loaded, which it shouldn't be.
+shell.send("runomni /twc/util/toggleNationalLDL.pyc 0\n") # disable ldl because it's probably still running from a previous instance
 
-# List of all LOT8s Flavors, with their respective runtimes.
+# Local on the 8s Flavor Length Lookup Table
 lot8s_times = {
     'S': 67,
     'M' : 200,
@@ -54,48 +63,42 @@ lot8s_times = {
     'D' : 100,
     'L' : 195
 }
+
 # Function for calculating time until LOT8s.
 def lot8s_time_calc(run_time: int) -> int:
-    time_slots = {
-        1: [18, 48],                     # modern
-        2: [28, 58],                     # future
-        3: [8, 18, 28, 38, 48, 58],      # classic
-        4: [0, 30]                       # half-hour
+    time_slots: Dict[int, List[int]] = {
+        1: [18, 48],                    # modern
+        2: [28, 58],                    # future
+        3: [8, 18, 28, 38, 48, 58],     # classic
+        4: [0, 30]                      # half-hour
     }
 
     now = datetime.datetime.now()
-    current_minute = now.minute
-    current_second = now.second
-    current_microsecond = now.microsecond
-    next_minutes = [m for m in time_slots[run_time] if m > current_minute or 
-                   (m == current_minute and (current_second > 0 or current_microsecond > 0))]
-    if next_minutes:
-        next_minute = next_minutes[0]
-        next_hour = now.hour
+    current_time = now.replace(microsecond=0)
+
+    for minute in sorted(time_slots[run_time]):
+        candidate = now.replace(minute=minute, second=0, microsecond=0)
+        if candidate >= current_time:
+            next_run = candidate
+            break
     else:
-        next_minute = time_slots[run_time][0]
-        next_hour = (now.hour + 1) % 24
-
-    next_run = now.replace(hour=next_hour, minute=next_minute, second=0, microsecond=0)
-    if next_run <= now: 
-        # This should never EVER happen.
-        print("WHAT THE FUCK")
-        raise Exception("congradulations you won the lottery")
-
+        next_run = now.replace(
+            hour=now.hour + 1,
+            minute=min(time_slots[run_time]),
+            second=0,
+            microsecond=0
+        )
+    
     delta = next_run - now
-    return int(delta.total_seconds() * 1000 + delta.microseconds / 1000)
+    return max(0, int(delta.total_seconds() * 1000))
 
+print("Setup done, Starting loop.")
 
-while True:
-    print("Starting LDL")
+while True: # Main Loop
     cmd(f"runomni /twc/util/toggleNationalLDL.pyc {ldl}") # Start LDL
-    time = lot8s_time_calc(run_time) # Calculate how much time until next LOT8s
-    print("next cue in", time) # inform user
-    time_py.sleep(time / 1000) # wait for next lot8s
-    print("cueing")
+    time = lot8s_time_calc(run_time); time_py.sleep(time / 1000) # Wait for next LOT8s Cue
     cmd(f"runomni /twc/util/toggleNationalLDL.pyc 0") # Stop LDL
     cmd(f"runomni /twc/util/load.pyc local {lot8s}") # Load LOT8s
-    time_py.sleep(0.1) # race condition fix?
+    time_py.sleep(0.1) # race condition fix
     cmd(f"runomni /twc/util/run.pyc local") # Run LOT8s
-    time = lot8s_times.get(lot8s)
-    time_py.sleep(time)
+    time = lot8s_times.get(lot8s); time_py.sleep(time) # Wait for LOT8s to finish
